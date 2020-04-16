ab9a7aa13775929d45788e1382186d488e84b9f9
Alfredo Salazar <>
2020-04-16 08:08:18 -0500

    Adding POST and DELETE

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
+const morgan = require( 'morgan' );
 
 const app = express();
+const jsonParser = bodyParser.json();
+
+app.use( morgan( 'dev' ) );
 
 let listOfStudents = [
     {
@@ -71,6 +76,68 @@ app.get( '/api/getStudentById/:id', ( req, res ) => {
     }
 
     return res.status( 200 ).json( result ); 
+});
+
+app.post( '/api/createStudent', jsonParser, ( req, res ) => {
+    console.log( "Adding a new student to the list." );
+    console.log( "Body ", req.body );
+
+    let name = req.body.name;
+    let id = req.body.id;
+
+    if( !id || !name ){
+        res.statusMessage = "One of these parameters is missing in the request: 'id' or 'name'.";
+        return res.status( 406 ).end();
+    }
+
+    if( typeof(id) !== 'number' ){
+        res.statusMessage = "The 'id' MUST be a number.";
+        return res.status( 409 ).end();
+    }
+    
+    let flag = true;
+
+    for( let i = 0; i < listOfStudents.length; i ++ ){
+        if( listOfStudents[i].id === id ){
+            flag = !flag;
+            break;
+        }
+    }
+
+    if( flag ){
+        let newStudent = { name, id };
+        listOfStudents.push( newStudent );
+
+        return res.status( 201 ).json( newStudent ); 
+    }
+    else{
+        res.statusMessage = "The 'id' is already on the student list.";
+        return res.status( 409 ).end();
+    }
+});
+
+app.delete( '/api/removeStudent', ( req, res ) => {
+    
+    let id = req.query.id;
+
+    if( !id ){
+        res.statusMessage = "Please send the 'id' to delete a student";
+        return res.status( 406 ).end();
+    }
+
+    let itemToRemove = listOfStudents.findIndex( ( student ) => {
+        if( student.id === Number( id ) ){
+            return true;
+        }
+    });
+
+    if( itemToRemove < 0 ){
+        res.statusMessage = "That 'id' was not found in the list of students.";
+        return res.status( 400 ).end();
+    }
+
+    listOfStudents.splice( itemToRemove, 1 );
+    return res.status( 204 ).end();
 });
 
 app.listen( 8080, () => {
