4d84e5949fd814c885e69d6d39c2579d9fc43145
Jesus Rivera Flores <A00820643@itesm.mx>
2020-05-15 09:51:15 -0500

    Changes to deploy

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/studentsdb';
+//                                     user         password                            database
+//exports.DATABASE_URL ="mongodb+srv://<username>:<password>@cluster0-wxjd9.mongodb.net/test?retryWrites=true&w=majority"
+//exports.DATABASE_URL = "mongodb+srv://admin2:admin@cluster0-wxjd9.mongodb.net/studentsdb?retryWrites=true&w=majority"
 exports.TOKEN = process.env.API_TOKEN || 'password12345';
 exports.PORT = process.env.PORT || '8080';
 
